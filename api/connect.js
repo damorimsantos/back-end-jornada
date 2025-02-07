a47c05d71149e88682f25772a287ec7622e0a77c
damorimsantos <diego@hashtagtreinamentos.com>
2025-02-07 17:14:00 -0300

    ajuste mongo

--- api/connect.js
+++ api/connect.js
@@ -1,16 +1,13 @@
-import "dotenv/config";
-import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+// dotenv.config({ path: "./.env" });
 
 const uri = process.env.ATLAS_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+const client = new MongoClient(uri);
+await client.connect();
 
 let database;
 
